3d54b6b7b212b830e7dabdfc5e886196e820891f
buddybuild <help@buddybuild.com>
2017-04-30 10:08:19 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- WorkoutCounter/AppDelegate.swift
+++ WorkoutCounter/AppDelegate.swift
@@ -12,6 +12,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         
         FIRApp.configure()
         startGenericControllers()
