0ba83ecf0e68b696fba07190bfd799cb5cba9cf8
Pavel Gurov <pegurov@avito.ru>
2020-06-20 10:29:20 +0300

    Remove BuddyBuild sdk

--- WorkoutCounter/AppDelegate.swift
+++ WorkoutCounter/AppDelegate.swift
@@ -12,10 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool
-    {
-        BuddyBuildSDK.setup()
-        
-        
+    {   
         FirebaseApp.configure()
         startGenericControllers()
         coreDataStack = CoreDataStackImp()
